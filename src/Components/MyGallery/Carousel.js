ba215973c868217f2ad1aad431cfdedf22871981
Kajol <kajol.katiyar@kiwitech.com>
2023-03-14 19:04:09 +0530

    Listing API

--- src/Components/MyGallery/Carousel.js
+++ src/Components/MyGallery/Carousel.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import slider from '../../slider.json'
 import Carousel from 'react-material-ui-carousel'
 import Item from './Items'
+import { imageListing_getData } from '../Services'
 
 const Carousel_Slider = () => {
+
+    useEffect(() => {
+        imageListing_getData().then((response) => {
+            console.log(response);
+        }).catch((error) => {
+            console.log(error);
+        });
+    });
+
+
     return (
         <Carousel >
             {
--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,28 +1,38 @@
 import axios from 'axios'
+import {accessToken} from './Constants'
 
-// const config = {
-//     headers: {
-//       'Authorization': 'Bearer ' + yourBearerToken
-//     }
-//   };
-//   const response = await axios.get(yourApiEndpoint, config);
-//   console.log(response.data);
-// }
 
-// This is to get tokens as response from the Sign-in API after the correct credentials are sent 
+const config = {
+    headers: {
+        "Content-Type": "application/json",
+        'Authorization': `Bearer ${ localStorage.getItem(accessToken)}`
+    }
+  };
+
+
+  export const imageListing_getData = async (data) => {
+    const response = await axios.get(`${process.env.REACT_APP_API}/image/image-gallery/`, config);
+
+    if (response.status === 200){
+        console.log(response.data);
+        return response.data;
+    }else {
+        console.log("Bad Request");
+    }
+    
+}
+
+/* This is to get tokens as response from the Sign-in API after the correct credentials are sent */
 export const signIn_postData = async (data) => {
-    const response = await axios.post(`${process.env.REACT_APP_API}/signin/`, data);
+    const response = await axios.post(`${process.env.REACT_APP_API}/user/signin/`, data);
     return response.data;
 }
 
 // This is to log-out the current logged-in user after the bearer is passed
 export const signOut_postData = async (data) => {
-    const response = await axios.post(`${process.env.REACT_APP_API}/sign-out/`,  data);
+    const response = await axios.post(`${process.env.REACT_APP_API}/user/sign-out/`, data);
     return response.data;
 }
 
 
-export const imageListing_getData = async (data) => {
-    const response = await axios.post(`${process.env.REACT_APP_API}/sign-out/`,  data);
-    return response.data;
-}
+
